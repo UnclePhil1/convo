9cea958bfac6094697f59804aa223e032a0f12f1
Unclephil <philipnssien@gmail.com>
2023-11-18 00:32:10 +0100

    removed photoupload

--- components/molecules/chatArea.tsx
+++ components/molecules/chatArea.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ImageUpload from "./profilePhoto";
 
 const ChartArea: React.FC = () => {
   return (
     <div>
       <div className="bg-white p-4 rounded-[30px] h-[80vh] w-[100%]">
-        <ImageUpload />
       </div>
     </div>
   );
